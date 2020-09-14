8841ab1a7921e3a947651bdbebf11d2ff7b700c6
Reggie <ryoboua@gmail.com>
2020-09-14 01:04:49 -0400

    Update constants.js

--- backend/constants.js
+++ backend/constants.js
@@ -1,6 +1,9 @@
 const FRAME_RATE = 500;
-const GRID_SIZE = 50;
-const STEP = 0.5;
+const GRID_SIZE = 12;
+const STEP = 5;
+const BORDER_PUSH_BACK = 5
+
+const Y_GRID_SIZE = 30
 
 module.exports = {
   FRAME_RATE,
