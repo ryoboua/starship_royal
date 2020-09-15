c06acd2680e5258c429f59b23bca6634cc2d74f9
Reggie <ryoboua@gmail.com>
2020-09-15 01:48:54 -0400

    Update Missiles.js

--- backend/classes/Missiles.js
+++ backend/classes/Missiles.js
@@ -11,4 +11,24 @@ module.exports = class Missiles {
   generateMissile(pos) {
     this.missiles.push(new Missile(pos))
   }
+
+  updateMissilePositions() {
+    if (!this.missiles.length) {
+      return
+    }
+    const missilesToRemove = []
+    this.missiles.forEach((mis, i) => {
+      const newPos = Vector.sub(mis.pos, mis.vel)
+      const newY = newPos.getY()
+      if (newY - GRID_SIZE < 0) {
+        missilesToRemove.push(i)
+      } else {
+        mis.pos = newPos
+      }
+    })
+
+    this.missiles = this.missiles.filter(
+      (mis, i) => !missilesToRemove.includes(i)
+    )
+  }
 }
