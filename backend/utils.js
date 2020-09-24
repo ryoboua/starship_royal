a5b9bf9dbb7745eaf45060f693e7d0ac348a9643
Reggie <ryoboua@gmail.com>
2020-09-24 16:41:56 -0400

    Update utils.js

--- backend/utils.js
+++ backend/utils.js
@@ -1,14 +1,12 @@
 module.exports = {
-    makeid,
-  }
-  
-  function makeid(length) {
-     var result           = '';
-     var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-     var charactersLength = characters.length;
-     for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-     }
-     return result;
-  }
-  
+  makeid: length => {
+    let result = ""
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    const charactersLength = characters.length
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result
+  },
+}
