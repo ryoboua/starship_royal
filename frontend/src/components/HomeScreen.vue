e56adf262a671816e5aa53aaa4ee1155d7f044ff
Reggie <ryoboua@gmail.com>
2020-10-15 23:06:27 -0400

    added error handling to homescreen

--- frontend/src/components/HomeScreen.vue
+++ frontend/src/components/HomeScreen.vue
@@ -5,13 +5,23 @@
     </div>
     <div class="homeScreen__menu">
       <div>
-        <input placeholder="Enter a name" type="text" v-model="name" />
+        <input
+          placeholder="Enter a name"
+          type="text"
+          v-model="name"
+          :class="{ 'input-error': errors.name }"
+        />
       </div>
       <h4>And</h4>
       <div>
         <button @click="handleNewGame">Create Game</button>
         <h4>Or</h4>
-        <input placeholder="Enter room code" v-model="roomName" type="text" />
+        <input
+          placeholder="Enter room code"
+          v-model="roomName"
+          type="text"
+          :class="{ 'input-error': errors.roomName }"
+        />
         <button @click="handleJoinGame">Join Game</button>
       </div>
     </div>
@@ -23,31 +33,54 @@ export default {
   name: "HomeScreen",
   data() {
     return {
-      roomName: null,
+      roomName: "",
       name: "",
+      errors: {
+        roomName: false,
+        name: false
+      }
+    };
+  },
+  watch: {
+    roomName: function() {
+      this.resetErrors();
+    },
+    name: function() {
+      this.resetErrors();
     }
   },
   methods: {
     handleNewGame() {
-      // if (this.name === "") {
-      //   return
-      // }
-      this.$socket.emit("NEW_GAME", this.name)
+      if (this.name === "") {
+        this.errors.name = true;
+        return;
+      }
+      this.$socket.emit("NEW_GAME", this.name);
     },
     handleJoinGame() {
-      // if (this.name === "") {
-      //   return
-      // }
-      // if (this.roomName === "") {
-      //   return
-      // }
+      if (this.name === "") {
+        this.errors.name = true;
+      }
+      if (this.roomName === "") {
+        this.errors.roomName = true;
+      }
+      if (this.errors.name || this.errors.roomName) {
+        return;
+      }
+
       this.$socket.emit("JOIN_GAME", {
         roomName: this.roomName,
-        name: this.name,
-      })
+        name: this.name
+      });
     },
+    resetErrors() {
+      this.errors = {
+        roomName: false,
+        name: false
+      };
+    }
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
