78098e5a040bad9da75f7362f31136fae3732d68
Reggie <ryoboua@gmail.com>
2020-10-19 00:24:06 -0400

    removed socket io

--- frontend_single_player/src/main.js
+++ frontend_single_player/src/main.js
@@ -1,27 +1,8 @@
 import Vue from "vue"
 import App from "./App.vue"
 import store from "./store"
-import VueSocketIO from "vue-socket.io"
 
 Vue.config.productionTip = false
-
-let url = "http://localhost:3000"
-
-if (process.env.NODE_ENV === "production") {
-  url = "http://159.203.17.103"
-}
-
-Vue.use(
-  new VueSocketIO({
-    debug: false,
-    connection: url,
-    vuex: {
-      store,
-      actionPrefix: "SOCKET_",
-      mutationPrefix: "SOCKET_",
-    },
-  })
-)
 
 new Vue({
   store,
