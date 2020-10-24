f890d1ea7f510d0cbf114b9f5f4c06903c7a7c39
Reggie <ryoboua@gmail.com>
2020-10-23 21:34:46 -0400

    restructured

---
+++ app/src/store/gameStore/gameMutations.js
@@ -0,0 +1,83 @@
+import {
+    createGame,
+    addPlayer,
+    removePlayer,
+    gameHandleKeyDown,
+    gameHandleKeyUp,
+    handleStartRound,
+    handleDeadPlayer
+} from "../../../game/controllers/gameController"
+import {
+    SET_GAME_TYPE,
+    CREATE_GAME,
+    START_ROUND,
+    START_GAME,
+    LOAD_LEVEL,
+    GAME_ACTIVE,
+    GAME_STATE_UPDATE,
+    KEY_DOWN,
+    KEY_UP,
+    COUNTDOWN,
+    DISPLAY_MSG,
+    ROUND_OVER,
+    GAME_OVER,
+    ADD_PLAYER,
+    REMOVE_PLAYER,
+    PLAYER_DEAD,
+} from "../../../appEvent"
+
+export default {
+    [SET_GAME_TYPE](state, type) {
+        state.type = type
+    },
+    [CREATE_GAME](state, { players, context }) {
+        state.players = players
+        state.game = createGame(players, context)
+    },
+    [START_ROUND](state, sequence) {
+        state.disableStartBtn = true
+        handleStartRound(state.game, sequence)
+    },
+    [LOAD_LEVEL](state, { level, initialGameState }) {
+        state.level = level;
+        state.timer = initialGameState.timer;
+        state.gameState = initialGameState
+    },
+    [GAME_ACTIVE](state, b) {
+        state.gameActive = b
+    },
+    [GAME_STATE_UPDATE](state, gameState) {
+        state.gameState = gameState
+        state.playerScores = gameState.playerScores;
+        state.timer = gameState.timer;
+    },
+    [KEY_DOWN](state, { keyCode, socketId }) {
+        gameHandleKeyDown(state.game, keyCode, socketId)
+    },
+    [KEY_UP](state, { keyCode, socketId }) {
+        gameHandleKeyUp(state.game, keyCode, socketId)
+    },
+    [COUNTDOWN](state, count) {
+        state.screen = count
+    },
+    [DISPLAY_MSG](state, msg) {
+        state.screen = msg
+    },
+    [ROUND_OVER](state, msg) {
+        state.screen = msg
+        state.disableStartBtn = false
+    },
+    [GAME_OVER](state, msg) {
+        state.screen = msg
+        state.disableStartBtn = false
+    },
+    [ADD_PLAYER](state, player) {
+        state.players = addPlayer(state.game, player)
+    },
+    [REMOVE_PLAYER](state, socketId) {
+        state.players = removePlayer(state.game, socketId)
+    },
+    [PLAYER_DEAD](state, socketId) {
+        handleDeadPlayer(state.game, socketId)
+    },
+}
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import Vuex from "vuex"
 import client from "./clientStore"
-import game from "./gameStore"
+import game from "./gameStore/"
 import modal from "./modalStore"
 Vue.use(Vuex)
 
