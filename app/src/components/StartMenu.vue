cb49d61b5036f8bb16b00f67d4c94b1eb436513b
Reggie <ryoboua@gmail.com>
2020-11-13 02:02:11 -0500

    Update StartMenu.vue

--- app/src/components/StartMenu.vue
+++ app/src/components/StartMenu.vue
@@ -88,14 +88,6 @@ export default {
       });
     },
   },
-  sockets: {
-    connect: function () {
-      console.log("socket connected");
-    },
-    disconnect: function () {
-      console.log("disconnect");
-    },
-  },
 };
 </script>
 
