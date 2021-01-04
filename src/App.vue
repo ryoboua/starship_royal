d215cbc30dc834546c7bfdcb44f6b8bd5c07802a
Reggie <ryoboua@gmail.com>
2021-01-03 20:53:09 -0500

    Added warning message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <StartMenu v-if="!roomName" />
+    <div v-if="width < 1200 || height < 600" class="warning-message">
+      <h3>
+        Starship Royal is meant to be played with a minimum screen width and
+        height of 1200px by 600px as well as with a physical keyword.
+      </h3>
+    </div>
+    <StartMenu v-else-if="!roomName" />
     <Main v-else />
 
     <Modal v-if="showModal" @close="closeModal">
@@ -23,13 +29,35 @@ export default {
     Main,
     Modal,
   },
+  data() {
+    return {
+      width: null,
+      height: null,
+    };
+  },
   computed: mapState({
     roomName: (state) => state.client.roomName,
     showModal: (state) => state.modal.showModal,
     header: (state) => state.modal.header,
     body: (state) => state.modal.body,
   }),
-  methods: mapMutations("modal", ["closeModal"]),
+  methods: {
+    ...mapMutations("modal", ["closeModal"]),
+    resizeEvent() {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+    },
+  },
+  beforeMount() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+  },
+  created() {
+    window.addEventListener("resize", this.resizeEvent);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.resizeEvent);
+  },
 };
 </script>
 
@@ -40,5 +68,13 @@ export default {
   height: 100vh;
   width: 100%;
   background-color: $primary-color;
+}
+
+.warning-message {
+  padding: 1em;
+  h3 {
+    margin-top: 100px;
+    line-height: 25px;
+  }
 }
 </style>
